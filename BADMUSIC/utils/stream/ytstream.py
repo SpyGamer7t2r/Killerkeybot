20e5db5793faf616ec7f8d1b5b8814870226b166
༄ᶦᶰᵈ᭄Sᴘʏ✿Gᴀᴍᴇʀ࿐ <gamingwithmayuresh7t@gmail.com>
2025-07-18 05:28:43 +0530

    Create ytstream.py

---
+++ BADMUSIC/utils/stream/ytstream.py
@@ -0,0 +1,24 @@
+from BADMUSIC.platforms.youtube import YouTube
+from BADMUSIC.utils.stream.stream import stream
+
+youtube = YouTube()
+
+async def yt_stream(client, message, query):
+    try:
+        result = await youtube.url(query)
+        if not result:
+            return await message.reply_text("No results found.")
+
+        return await stream(
+            client,
+            message,
+            title=result["title"],
+            videoid=result["videoid"],
+            user_id=message.from_user.id,
+            duration=result["duration_min"],
+            streamtype="youtube",
+            playmode="Audio",
+            query=query,
+        )
+    except Exception as e:
+        await message.reply_text(f"❌ Error: {e}")

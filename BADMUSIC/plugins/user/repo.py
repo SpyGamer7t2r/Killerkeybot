164bae44d8af0656e580147b7322c9caa775268d
༄ᶦᶰᵈ᭄Sᴘʏ✿Gᴀᴍᴇʀ࿐ <gamingwithmayuresh7t@gmail.com>
2025-07-18 01:23:08 +0530

    Update repo.py

--- BADMUSIC/plugins/user/repo.py
+++ BADMUSIC/plugins/user/repo.py
@@ -9,23 +9,23 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 async def start(client: Client, message: Message):
     await message.reply_video(
         video=f"https://telegra.ph/file/bda2c51bd00c8f4710b04.mp4",
-        caption=f"❤️ ʜᴇʏ {message.from_user.mention} [☆ ʀᴇᴘᴏ 💗](https://github.com/Badhacker98/BADMUSIC/fork)",
+        caption=f"❤️ ʜᴇʏ {message.from_user.mention} [☆ ʀᴇᴘᴏ 💗](https://t.me/dark_x_knight_musiczz_support)",
         reply_markup=InlineKeyboardMarkup(
             [
                [
             InlineKeyboardButton(
-                text="☆ ᴏᴡɴᴇʀ 💗 ", url=f"https://t.me/ll_BAD_MUNDA_ll"
+                text="☆ ᴏᴡɴᴇʀ 💗 ", url=f"https://t.me/dark_x_knight_musiczz_support"
             ),
             InlineKeyboardButton(
-                text="☆ ɢʀᴏᴜᴘ 💗", url=f"https://t.me/PBX_CHAT"
+                text="☆ ɢʀᴏᴜᴘ 💗", url=f"https://t.me/+wWtbIiAGjUI2YzI1"
             ),
         ],
           [
             InlineKeyboardButton(
-                text="☆ ᴄʜᴀɴɴᴇʟ 💗 ", url=f"https://t.me/HEROKUBIN_01"
+                text="☆ ᴄʜᴀɴɴᴇʟ 💗 ", url=f"https://t.me/dark_x_knight_musiczz_support"
             ),
             InlineKeyboardButton(
-                text="☆ ʀᴇᴘᴏ 💗", url=f"https://github.com/Badhacker98/BADMUSIC/fork"
+                text="☆ ʀᴇᴘᴏ 💗", url=f"https://t.me/+wWtbIiAGjUI2YzI1"
             ),
         ],
                 [

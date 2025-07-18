d9de1b05801ce2d0c704e5ef2bb485b4c6f7b9c7
༄ᶦᶰᵈ᭄Sᴘʏ✿Gᴀᴍᴇʀ࿐ <gamingwithmayuresh7t@gmail.com>
2025-07-18 20:49:06 +0530

    Update ytstream.py

--- BADMUSIC/utils/stream/ytstream.py
+++ BADMUSIC/utils/stream/ytstream.py
@@ -5,15 +5,15 @@ youtube = YouTube()
 
 async def yt_stream(client, message, query):
     try:
-        result = await youtube.url(query)
-        if not result:
-            return await message.reply_text("No results found.")
+        result = await youtube.track(query)
+        if "error" in result:
+            return await message.reply_text(result["error"])
 
         return await stream(
             client,
             message,
             title=result["title"],
-            videoid=result["videoid"],
+            videoid=result["id"],
             user_id=message.from_user.id,
             duration=result["duration_min"],
             streamtype="youtube",
